f216c767e8a670bf4d6846b55cdf928adb69dc92
ashhar-sartaj <ashharsartaj04@gmail.com>
2026-02-14 23:12:40 +0530

    /apps/api/package.json: removed engine: nodeversion. /apps/api/src/dev removed await in server creation.

--- apps/api/src/dev.ts
+++ apps/api/src/dev.ts
@@ -1,6 +1,6 @@
 import { createServer } from './server.js'
 
-const app =  await createServer();
+const app =  createServer();
 const port=4000;
 app.listen({ port, host: '0.0.0.0' }, () => {
     console.log('app is running on port: ', port);
